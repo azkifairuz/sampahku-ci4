47473b862437a1e3a98733b6d27a6e321e78ebf7
azkifairuz <azkiajmal@gmail.com>
2023-06-11 01:34:13 +0700

    responsive navbar user

--- app/Views/layout/v_header-user.php
+++ app/Views/layout/v_header-user.php
@@ -10,7 +10,7 @@
     <title></title>
   </head>
   <body class="font-poppins">
-  <nav class="w-full flex justify-between px-10 items-center">
+  <nav class="w-full hidden md:flex justify-between px-10 items-center">
     <img src="asset/Logo.png" alt="logo sampaku" class=" mx-auto md:mx-0">
     <ul class="text-lg flex gap-5">
       <li class="cursor-pointer">
@@ -27,5 +27,23 @@
       </li>
     </ul>
   </nav>
+  
+  <nav class="w-full bg-greenMain fixed bottom-0 right-0 h-24 left-0 flex md:hidden justify-between px-10 items-center">
+    <ul class="text-lg flex items-center w-full text-white justify-evenly gap-5">
+      <li class="cursor-pointer">
+        Beranda
+      </li>
+      <li class="cursor-pointer">
+        kategori
+      </li >
+      <li class="cursor-pointer">
+        mitra
+      </li>
+      <li class="cursor-pointer">
+        Akun
+      </li>
+    </ul>
+  </nav>
+  
   </body>
 </html>
