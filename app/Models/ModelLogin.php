f8034ae7fe284bc1d4b9bb0b8b689bd20558fb0c
azkifairuz <azkiajmal@gmail.com>
2023-06-12 22:06:18 +0700

    create method cek login

--- app/Models/ModelLogin.php
+++ app/Models/ModelLogin.php
@@ -7,10 +7,9 @@ use CodeIgniter\Model;
 class ModelLogin extends Model
 {
    
-    public function getDataId($table,$where)
+    public function getDataId($table,$username)
     {
-        $builder = $this->db->table($table);
-        $builder->where($where);
-        return $builder->get()->getResult();
+        $builder = $this->db->query("SELECT * FROM `auth` WHERE `username` = '$username' AND `roles` = 'customer'");
+        return $builder->getResult();
     }
 }
