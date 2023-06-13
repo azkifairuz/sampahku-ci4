a7710d8daa2d18d639179f4aa0ef635591073047
azkifairuz <azkiajmal@gmail.com>
2023-06-13 15:38:40 +0700

    model withdraw

--- app/Models/ModelWithdraw.php
+++ app/Models/ModelWithdraw.php
@@ -6,36 +6,10 @@ use CodeIgniter\Model;
 
 class ModelWithdraw extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'modelwithdraws';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
-
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    public function withdrawCoin($data) {
+        $withdraw = $this->db->table('auth')
+        ->insert($data);
+  
+      return $withdraw;
+    }
 }
